330c1b1a89683616e72a974c314e2052aba99899
HildebrandoLima <hildebrandolima16@gmail.com>
2024-12-20 16:52:12 -0300

    Adicionado cabeçalho na requição

--- src/config/api.js
+++ src/config/api.js
@@ -4,6 +4,9 @@ import env from './env';
 const api = axios.create({
   baseURL: env.API_BASE_URL,
   timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
